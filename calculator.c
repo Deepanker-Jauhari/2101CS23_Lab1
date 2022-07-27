9387fbb5ba45004cbdb4809a56edeb72332d2ece
DeeJay <deepanker2101cs23_iitp.ac.in>
2022-07-27 20:15:07 +0530

    added Addition operation

---
+++ calculator.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include <math.h>
+
+int main() {
+    char operator;
+    double num1, num2;
+    printf("Enter the operator (+,-,*,/,pow): ");
+    scanf("%c", &operator);
+    printf("Enter the two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch(operator) {
+        case '+':
+        printf("%lf + %lf = %lf", num1, num2, num1+num2);
+        break;
+
+        default:
+        printf("Wrong Operator");
+    }
+
+    return 0;
+}
