fec707b8902b9c3a9b3990544e0d7091d64f60b0
DeeJay <deepanker2101cs23_iitp.ac.in>
2022-07-27 21:06:25 +0530

    Added Selection Sorter

--- sorter.c
+++ sorter.c
@@ -16,6 +16,26 @@ void insertionSort(int arr[], int n)
     }
 }
 
+void swap(int *x, int *y)
+{
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+  
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_index;
+    for (i = 0; i < n-1; i++) {
+        min_index = i;
+        for (j = i+1; j < n; j++)
+          if (arr[j] < arr[min_index])
+            min_index = j;
+
+        swap(&arr[min_index], &arr[i]);
+    }
+}
+
 void printArray(int arr[], int n)
 {
     int i;
@@ -45,6 +65,11 @@ int main() {
         case 1:
         insertionSort(arr, n);
         printArray(arr,n);
+        break;
+
+        case 2:
+        selectionSort(arr, n);
+        printArray(arr, n);
         break;
 
         default:
